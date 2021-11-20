6d77e481ca8544ae6e4ca6cd445648019dcf7020
nmzgnv <nick-mozg1@mail.ru>
2021-11-20 21:59:46 +0500

    Update: docker

--- config.py
+++ config.py
@@ -1,6 +1,8 @@
 import os
 
-REDIS_URL = f"redis://127.0.0.1:6379"
+REDIS_HOST = os.getenv('REDIS_HOST', "localhost")
+REDIS_PORT = os.getenv('REDIS_PORT', 6379)
+REDIS_URL = f"redis://{REDIS_HOST}:{REDIS_PORT}"
 
 HOST = os.getenv('HOST', '127.0.0.1')
 PORT = int(os.getenv('PORT', 8000))
