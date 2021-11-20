7ab2497806d5f6c8f303e17899629a09693fc508
nmzgnv <nick-mozg1@mail.ru>
2021-11-20 18:06:48 +0500

    Update: database handler

--- handlers/database_handler.py
+++ handlers/database_handler.py
@@ -1,15 +1,70 @@
+import json
+from typing import List
+
 from aiohttp import web
 from aiohttp_pydantic import PydanticView
 from pydantic import BaseModel, Field
 
-from handlers.utils import get_error_json
+from handlers.utils import get_error_response
+
+
+class ExchangeModel(BaseModel):
+    from_: str = Field(alias='from')
+    to: str
+    exchange_rate: float = Field(gt=0)
 
 
 class DatabaseHandlerModel(BaseModel):
     merge: int = Field(ge=0, le=1)
+    data: List[ExchangeModel]
 
 
 class DatabaseHandler(PydanticView):
+    def parse_post_body(self, params: DatabaseHandlerModel) -> dict:
+        """
+        :param params:
+             {
+                "merge": 1,
+                "data": [{
+                    "from": "RUB",
+                    "to": "UAH",
+                    "exchange_rate": 1
+                }]
+            }
+        :return: dict with combined params
+        """
+        exchanges_dict = {}
+        for exchange in params.data:
+            from_ = exchange.from_
+            to = exchange.to
+            rate = exchange.exchange_rate
+
+            if from_ in exchanges_dict.keys():
+                exchanges_dict[from_][to] = rate
+            else:
+                exchanges_dict[from_] = {to: rate}
+        return exchanges_dict
+
     async def post(self, params: DatabaseHandlerModel):
-        print(params)
-        return web.json_response({'name': 'good'})
+        db = self.request.app.get('db')
+        exchanges_dict = self.parse_post_body(params)
+
+        async with db.pipeline(transaction=True) as pipe:
+            for currency in exchanges_dict.keys():
+                data_to_save = exchanges_dict[currency]
+
+                if params.merge:
+                    old_data = await db.get(currency)
+                    if old_data:
+                        old_data = json.loads(old_data)
+                        old_data.update(data_to_save)
+                        data_to_save = old_data
+
+                await pipe.set(currency, json.dumps(data_to_save))
+
+            results = await pipe.execute()
+            if not all(results):
+                return get_error_response('Transactions to the database failed')
+
+        answer = [i.json() for i in params.data]
+        return web.json_response({'data': answer})
--- handlers/utils.py
+++ handlers/utils.py
@@ -3,5 +3,5 @@ from typing import Optional
 from aiohttp import web
 
 
-def get_error_json(text: str, status: Optional[int] = 400):
+def get_error_response(text: str, status: Optional[int] = 400):
     return web.json_response({'error': text}, status=status)
